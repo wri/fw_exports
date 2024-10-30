69bd8d9fe58acd692381b765b8a52d921fa86eda
Burhanuddin Salim <salim@3sidedcube.com>
2024-10-30 15:36:29 +0000

    Set font back to regular after rendering burmese

--- app/src/services/reportFile.service.js
+++ app/src/services/reportFile.service.js
@@ -540,10 +540,9 @@ class ReportFileService {
           }
         } else textToPrint = value;
 
-        doc
-          .font(isBurmese(textToPrint) ? "Burmese" : "Regular")
-          .fontSize(13)
-          .text(textToPrint, 80 + 250 * (i % 2), 170 + ((i - (i % 2)) / 2) * 50);
+        doc.font(isBurmese(textToPrint) ? "Burmese" : "Regular")
+        doc.fontSize(13).text(textToPrint, 80 + 250 * (i % 2), 170 + ((i - (i % 2)) / 2) * 50);
+        doc.font("Regular")
       });
 
       doc.moveDown(1);
